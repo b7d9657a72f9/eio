ac43fcc7830bc10c7194e22f892fad727dc76739
b7d9657a72f9 <mshomebook@gmail.com>
2025-11-01 12:21:41 -0700

    Beta2

--- Beta2.java
+++ Beta2.java
@@ -8,10 +8,11 @@ import java.util.Scanner;
 class Beta2 {
     public static void main(String[] args) {
         try (Scanner SourceReader = new Scanner(args[0])) {
-
+            while (SourceReader.hasNextLine()) {
+                System.out.println(SourceReader.nextLine());
+            }
         } catch (Exception e) {
             System.out.println("An error occured. :(");
-            
         }
     }
 }
